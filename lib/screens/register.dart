76c76c717ed877ab2d26d2391a1a0464373af1b4
XessIT <nasreenxess@gmail.com>
2024-08-05 15:32:09 +0530

    complete registration

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,12 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-import '../bloc/Register/Register_event.dart';
-import '../bloc/login/login_bloc.dart';
-import '../bloc/login/login_event.dart';
-import '../bloc/login/login_state.dart';
+import '../bloc/register/register_event.dart';
 import '../bloc/register/register_bloc.dart';
 import '../bloc/register/register_state.dart';
 import '../location.dart';
@@ -26,7 +21,7 @@ class RegisterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
 
-    Future<void> _pickLocation() async {
+    Future<void> pickLocation() async {
       LatLng? location = await Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => LocationPicker()),
@@ -46,10 +41,10 @@ class RegisterScreen extends StatelessWidget {
           padding: const EdgeInsets.only(top: 70),
           child: Column(
             children: [
-              Align(
+            const Align(
                 alignment: Alignment.topLeft,
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
                   child: Text(
                     'Welcome Back !',
                     style: TextStyle(
@@ -60,14 +55,14 @@ class RegisterScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               Expanded(
                 child: Align(
                   alignment: Alignment.bottomCenter,
                   child: Container(
                     width: double.infinity,
                     height: screenHeight * 0.80, // Adjust height based on screen height
-                    decoration: BoxDecoration(
+                    decoration: const BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(40),
@@ -79,7 +74,7 @@ class RegisterScreen extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
                           SizedBox(height: screenHeight * 0.05),
-                          Text('Sign Up', style: TextStyle(fontSize: 24,)),
+                          const Text('Sign Up', style: TextStyle(fontSize: 24,)),
 
                           SizedBox(
                             child: Column(
@@ -89,7 +84,7 @@ class RegisterScreen extends StatelessWidget {
                                   padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                                   child: TextField(
                                     controller: firstNameController,
-                                    decoration: InputDecoration(
+                                    decoration: const InputDecoration(
                                       labelText: 'First Name',
                                       border: OutlineInputBorder(),
                                       prefixIcon: Icon(Icons.person,color: Color(0xFF22538D),),
@@ -100,12 +95,11 @@ class RegisterScreen extends StatelessWidget {
                                   padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                                   child: TextField(
                                     controller: lastNameController,
-                                    decoration: InputDecoration(
+                                    decoration: const InputDecoration(
                                       labelText: 'Last Name',
                                       border: OutlineInputBorder(),
                                       prefixIcon: Icon(Icons.person,color: Color(0xFF22538D),),
                                     ),
-                                    obscureText: true,
                                   ),
                                 ),
                                 Padding(
@@ -114,7 +108,7 @@ class RegisterScreen extends StatelessWidget {
                                     controller: addressController,
                                     maxLength: 200,
                                     //maxLines: 3,
-                                    decoration: InputDecoration(
+                                    decoration: const InputDecoration(
                                       labelText: 'Address',
                                       border: OutlineInputBorder(),
                                       prefixIcon: Icon(Icons.location_on,color: Color(0xFF22538D),),
@@ -129,19 +123,18 @@ class RegisterScreen extends StatelessWidget {
                                   padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                                   child: TextField(
                                     controller: mobileController,
-                                    decoration: InputDecoration(
+                                    decoration: const InputDecoration(
                                       labelText: 'Mobile Number',
                                       border: OutlineInputBorder(),
                                       prefixIcon: Icon(Icons.phone_android,color: Color(0xFF22538D),),
                                     ),
-                                    obscureText: true,
                                   ),
                                 ),
                                 Padding(
                                   padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                                   child: TextField(
                                     controller: otpController,
-                                    decoration: InputDecoration(
+                                    decoration: const InputDecoration(
                                       labelText: 'OTP',
                                       border: OutlineInputBorder(),
                                       prefixIcon: Icon(Icons.password,color: Color(0xFF22538D),),
@@ -153,7 +146,7 @@ class RegisterScreen extends StatelessWidget {
                                   padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                                   child: TextField(
                                     controller: passwordController,
-                                    decoration: InputDecoration(
+                                    decoration: const InputDecoration(
                                       labelText: 'Password',
                                       border: OutlineInputBorder(),
                                       prefixIcon: Icon(Icons.phonelink_lock,color: Color(0xFF22538D),),
@@ -162,18 +155,20 @@ class RegisterScreen extends StatelessWidget {
                                   ),
                                 ),
                                 SizedBox(height: screenHeight * 0.05),
-                                BlocConsumer<RegisterBloc,RegisterState>(
+                                BlocConsumer<RegisterBloc, RegisterState>(
                                   listener: (context, state) {
                                     if (state is RegisterFailure) {
                                       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(state.error)));
                                     } else if (state is RegisterSuccess) {
                                       // Navigate to home screen
-                                      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomeScreen(welcomeMessage: 'Welcome to CMS, where your convenience is our commitment!')));
+                                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                                        builder: (context) => HomeScreen(welcomeMessage: 'Welcome to CMS, where your convenience is our commitment!'),
+                                      ));
                                     }
                                   },
                                   builder: (context, state) {
                                     if (state is RegisterLoading) {
-                                      return CircularProgressIndicator();
+                                      return const CircularProgressIndicator();
                                     }
 
                                     return Column(
@@ -191,27 +186,28 @@ class RegisterScreen extends StatelessWidget {
                                             width: double.infinity,
                                             height: 50,
                                             child: MaterialButton(
-                                              color: Color(0xFF22538D),
+                                              color: const Color(0xFF22538D),
                                               onPressed: () {
-                                                // BlocProvider.of<RegisterBloc>(context).add(
-                                                //   RegisterButtonPressed(
-                                                //     firstName: firstNameController.text,
-                                                //     lastName: lastNameController.text,
-                                                //     address: addressController.text,
-                                                //     mobile: mobileController.text,
-                                                //     otp: otpController.text,
-                                                //     password: passwordController.text,
-                                                //   ),
-                                                // );
+                                                BlocProvider.of<RegisterBloc>(context).add(
+                                                  RegisterButtonPressed(
+                                                    firstName: firstNameController.text,
+                                                    lastName: lastNameController.text,
+                                                    address: addressController.text,
+                                                    mobile: mobileController.text,
+                                                    otp: otpController.text,
+                                                    password: passwordController.text,
+                                                  ),
+                                                );
                                               },
-                                              child: Text('REGISTER', style: TextStyle(color: Colors.white, fontSize: 18)),
+                                              child: const Text('REGISTER', style: TextStyle(color: Colors.white, fontSize: 18)),
                                             ),
                                           ),
                                         ),
                                       ],
                                     );
                                   },
-                                ),
+                                )
+
                               ],
                             ),
                           ),
